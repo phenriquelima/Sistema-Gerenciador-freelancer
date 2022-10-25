3464665e85a3bc6af49eb276029acc76728445a4
Henrique Lima <phenrique.lima@outlook.com.br>
2022-10-25 15:51:27 +0000

    Ajustando extensoes do php

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN apt-get update
 
 RUN apt-get install -y git zip unzip libzip-dev zlib1g-dev libmcrypt-dev libpng-dev nano \
  && apt-get install --yes --force-yes cron g++ gettext libicu-dev openssl libmcrypt-dev libc-client-dev libkrb5-dev  libxml2-dev libfreetype6-dev libgd-dev libmcrypt-dev bzip2 libbz2-dev libtidy-dev libcurl4-openssl-dev libz-dev libmemcached-dev libxslt-dev \
- && docker-php-ext-install mysql gd mysqli zip mcrypt \
+ && docker-php-ext-install mysql gd mysqli zip mcrypt pdo pdo_mysql\
  && docker-php-ext-enable mysql \
  && docker-php-ext-configure gd --with-freetype-dir=/usr --with-jpeg-dir=/usr --with-png-dir=/usr
 
--- application/config/database.php
+++ application/config/database.php
@@ -13,13 +13,13 @@
 							$active_group = 'default';
 							$active_record = TRUE;
 
-							$db['default']['hostname'] = 'localhost';
-							$db['default']['username'] = 'root';
-							$db['default']['password'] = '';
-							$db['default']['database'] = 'FC2';
+							$db['default']['hostname'] = '192.168.1.100';
+							$db['default']['username'] = 'freelancer';
+							$db['default']['password'] = '88c1f0784';
+							$db['default']['database'] = 'freelancer';
 							$db['default']['dbdriver'] = 'mysql';
 							$db['default']['dbprefix'] = '';
-							$db['default']['pconnect'] = FALSE;
+							$db['default']['pconnect'] = TRUE;
 							$db['default']['db_debug'] = TRUE;
 							$db['default']['cache_on'] = FALSE;
 							$db['default']['cachedir'] = '';
